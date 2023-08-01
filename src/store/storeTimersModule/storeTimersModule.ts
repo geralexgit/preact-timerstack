31e1f855e70d91edd5177dd5165ad4d37105d498
Alex Grsmv <geralex@internet.ru>
2023-08-01 17:42:51 +0300

    storeon fixed

--- src/store/storeTimersModule/storeTimersModule.ts
+++ src/store/storeTimersModule/storeTimersModule.ts
@@ -3,9 +3,9 @@ import { StoreonModule } from 'storeon'
 import { State, Events } from '../storeTypes'
 
 export const storeTimersModule: StoreonModule<State, Events> = (store) => {
-	store.on('@init', (state) => {
-		timers: [{ id: 1, name: 'string', duration: 10 }]
-	})
+	store.on('@init', (state) => ({
+		timers: [{ id: 1, name: 'string', duration: 10 }],
+	}))
 	store.on('AddTimer', (state, event) => ({
 		timers: [...state.timers, event],
 	}))
