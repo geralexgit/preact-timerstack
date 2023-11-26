e6421f3dff2c7f82878ccf691491f63620141ddf
Alex Grsmv <geralex@internet.ru>
2023-11-26 17:58:45 +0300

    tailwind css connected

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import './style/index.css';
-import App from './components/app';
+// import 'tailwindcss/dist/tailwind.min.css'
+import './index.css'
+import App from './components/app'
 
-export default App;
+export default App
